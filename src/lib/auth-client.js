d16a8f7cccc5312ed38b70516c6027901434326a
Sima-akhter <simaakter5301@gmail.com>
2026-06-29 16:25:40 +0600

    deploy ment esu resolved

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -2,7 +2,11 @@ import { createAuthClient } from "better-auth/react";
 import { inferAdditionalFields } from "better-auth/client/plugins";
 
 export const authClient = createAuthClient({
-  baseURL: process.env.NEXT_PUBLIC_APP_URL || (typeof window !== "undefined" ? window.location.origin : "http://localhost:3000"),
+  baseURL:
+    process.env.NEXT_PUBLIC_APP_URL ||
+    (typeof window !== "undefined"
+      ? window.location.origin
+      : "https://medi-care-client-seven.vercel.app"),
   plugins: [
     inferAdditionalFields({
       user: {
@@ -13,8 +17,8 @@ export const authClient = createAuthClient({
         status: {
           type: "string",
           defaultValue: "active",
-        }
-      }
-    })
-  ]
+        },
+      },
+    }),
+  ],
 });
